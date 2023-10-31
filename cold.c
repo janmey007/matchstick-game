b3cb6ff72b48b36ee7a423bcbb86c62310bd35dc
janmey007 <patiljanmey562@gmail.com>
2023-10-31 14:27:14 +0530

    first commit

---
+++ cold.c
@@ -0,0 +1,44 @@
+#include "stdio.h"
+int main(int argc, char const* argv[])
+{
+	printf("this is a game when computer wins every time . try it i swear you can't beat my program\n");
+	printf("choose the number of sticks here\n");
+	int j, a, k = 0;
+
+start:
+	printf("please type the value between 1 and 4\n");
+	printf(" computer played his move and the current value of k is %d and computer picked up %d sticks \a\n", k, j);
+	scanf("%d", &a);
+	if (!((a > 0) && (a < 5)))
+	{
+		printf("you selected non favourable value so fuck off and  get back to where you were \n");
+		goto start;
+	}
+	if ((k + a) > 21)
+	{
+		printf("you selected non favourable value so fuck off and get back to where you were\n");
+		goto start;
+	}
+	else
+	{
+
+		k += a;
+		for (j = 1; j < 5; j++)
+		{
+
+			if (((k + j) % 5) == 0)
+			{
+				k += j;
+				goto start;
+			}
+			else if (k == 21)
+			{
+				goto end;
+			}
+		}
+	}
+
+end:
+	printf("%d completed and computer won this game\n", k);
+	return 0;
+}
